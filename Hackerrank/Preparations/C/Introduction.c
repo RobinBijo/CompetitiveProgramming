6f34e181dcb3dca57b2a1a91a5230172f0ac7613
R0b1n 8!]0 <69898405+RobinBijo@users.noreply.github.com>
2024-11-20 18:48:37 +0530

    Added Playing with Characters

--- Hackerrank/Preparations/C/Introduction.c
+++ Hackerrank/Preparations/C/Introduction.c
@@ -1,4 +1,4 @@
-//Hello World! in C
+// PROGRAM 1 - Hello World! in C
 #include <stdio.h>
 #include <string.h>
 #include <math.h>
@@ -16,3 +16,26 @@ int main()
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
     return 0;
 }
+//-------------------------------------------------------------------------------
+//Playing With Characters
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() 
+{
+    char ch;
+    char s[100];
+    char sen[100];
+    scanf("%c\n",&ch);
+    scanf("%s\n",&s);    
+    scanf("%[^\n]%*c",&sen);
+    printf("%c\n",ch);
+    printf("%s\n",s);
+    printf("%s\n",sen);
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+    return 0;
+}
+//-------------------------------------------------------------------------------
