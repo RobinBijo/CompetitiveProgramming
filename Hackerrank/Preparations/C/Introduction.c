675f867268f8862307056a05096eae6357b5c0b7
R0b1n 8!]0 <69898405+RobinBijo@users.noreply.github.com>
2024-11-20 18:50:37 +0530

    Added Sum and Difference of Two Numbers

--- Hackerrank/Preparations/C/Introduction.c
+++ Hackerrank/Preparations/C/Introduction.c
@@ -17,7 +17,7 @@ int main()
     return 0;
 }
 //-------------------------------------------------------------------------------
-//Playing With Characters
+//PROGRAM 2 - Playing With Characters
 #include <stdio.h>
 #include <string.h>
 #include <math.h>
@@ -39,3 +39,25 @@ int main()
     return 0;
 }
 //-------------------------------------------------------------------------------
+//PROGRAM 3 - Sum and Difference of Two Numbers
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+    int m;int n;
+    float k; float l;
+    scanf("%d %d",&m,&n);
+    scanf("%f %f",&k,&l);
+    int Iadd = m+n;
+    int Isub = m-n;
+    float fadd = k+l;
+    float fsub = k-l;
+    printf("%d %d\n",Iadd,Isub);
+    printf("%0.1f %0.1f",fadd,fsub);// Rounding off to one decimal point
+	
+    
+    return 0;
+}
